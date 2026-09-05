806ccdd9f8b952c37df331d02a240bf0da9e8de0
Joseph Micheli <josephwmicheli@gmail.com>
2026-09-05 18:57:18 -0500

    Add slang example to image

--- examples/image/main.rs
+++ examples/image/main.rs
@@ -574,6 +574,7 @@ fn window_size_dependent_setup(
 }
 
 mod vs {
+    #[cfg(not(feature = "use-slang"))]
     vulkano_shaders::shader! {
         ty: "vertex",
         src: r"
@@ -588,9 +589,31 @@ mod vs {
             }
         ",
     }
+
+    #[cfg(feature = "use-slang")]
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        lang: "slang",
+        src: r#"
+            struct VSOutput {
+                float4 position : SV_Position;
+                float2 tex_coords;
+            };
+
+            [shader("vertex")]
+            VSOutput main(float2 position) {
+                VSOutput output;
+
+                output.position = float4(position, 0.0, 1.0);
+                output.tex_coords = position + float2(0.5);
+                return output;
+            }
+        "#,
+    }
 }
 
 mod fs {
+    #[cfg(not(feature = "use-slang"))]
     vulkano_shaders::shader! {
         ty: "fragment",
         src: r"
@@ -606,5 +629,20 @@ mod fs {
                 f_color = texture(sampler2D(tex, s), tex_coords);
             }
         ",
+    }
+
+    #[cfg(feature = "use-slang")]
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        lang: "slang",
+        src: r#"
+            SamplerState s;
+            Texture2D tex;
+
+            [shader("fragment")]
+            float4 main(float2 tex_coords) {
+                return tex.Sample(s, tex_coords);
+            }
+        "#,
     }
 }
