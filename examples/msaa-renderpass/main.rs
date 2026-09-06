da99d8e823cd159c79b87f408eeeed3a813a3a09
Joseph Micheli <josephwmicheli@gmail.com>
2026-09-05 19:06:27 -0500

    Add slang example to msaa-renderpass

--- examples/msaa-renderpass/main.rs
+++ examples/msaa-renderpass/main.rs
@@ -242,6 +242,7 @@ fn main() {
     // and save it to a PNG file.
 
     mod vs {
+        #[cfg(not(feature = "use-slang"))]
         vulkano_shaders::shader! {
             ty: "vertex",
             src: r"
@@ -254,9 +255,21 @@ fn main() {
                 }
             ",
         }
+
+        #[cfg(feature = "use-slang")]
+        vulkano_shaders::shader! {
+            ty: "vertex",
+            lang: "slang",
+            src: r"
+                float4 main(float2 position) : SV_Position {
+                    return float4(position, 0.0, 1.0);
+                }
+            ",
+        }
     }
 
     mod fs {
+        #[cfg(not(feature = "use-slang"))]
         vulkano_shaders::shader! {
             ty: "fragment",
             src: r"
@@ -266,6 +279,17 @@ fn main() {
 
                 void main() {
                     f_color = vec4(1.0, 0.0, 0.0, 1.0);
+                }
+            ",
+        }
+
+        #[cfg(feature = "use-slang")]
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            lang: "slang",
+            src: r"
+                float4 main() {
+                    return float4(1.0, 0.0, 0.0, 1.0);
                 }
             ",
         }
